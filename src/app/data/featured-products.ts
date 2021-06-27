443547ac776dd1a74c39575fdceadd7a2825ef82
Dillon LeDoux <dillon.ledoux@me.com>
2021-06-27 00:16:46 -0500

    homepage ready for mobile

--- src/app/data/featured-products.ts
+++ src/app/data/featured-products.ts
@@ -4,21 +4,21 @@ export const FEATURED_ITEMS: FeaturedItem[] = [
     {
         name: 'Marshmello OG',
         link: '/test',
-        photo: 'https://s3.amazonaws.com/www.jumpnbeanpharms.net/images/white-daisy.jpg'
+        photo: 'https://play-lh.googleusercontent.com/IeNJWoKYx1waOhfWF6TiuSiWBLfqLb18lmZYXSgsH1fvb8v1IYiZr5aYWe0Gxu-pVZX3=s360-rw'
     },
     {
         name: 'Marshmello 2',
         link: '/test',
-        photo: 'https://s3.amazonaws.com/www.jumpnbeanpharms.net/images/white-daisy.jpg'
+        photo: 'https://play-lh.googleusercontent.com/IeNJWoKYx1waOhfWF6TiuSiWBLfqLb18lmZYXSgsH1fvb8v1IYiZr5aYWe0Gxu-pVZX3=s360-rw'
     },
     {
         name: 'Marshmello 3',
         link: '/test',
-        photo: 'https://s3.amazonaws.com/www.jumpnbeanpharms.net/images/white-daisy.jpg'
+        photo: 'https://play-lh.googleusercontent.com/IeNJWoKYx1waOhfWF6TiuSiWBLfqLb18lmZYXSgsH1fvb8v1IYiZr5aYWe0Gxu-pVZX3=s360-rw'
     },
     {
         name: 'Marshmello 4',
         link: '/test',
-        photo: 'https://s3.amazonaws.com/www.jumpnbeanpharms.net/images/white-daisy.jpg'
+        photo: 'https://play-lh.googleusercontent.com/IeNJWoKYx1waOhfWF6TiuSiWBLfqLb18lmZYXSgsH1fvb8v1IYiZr5aYWe0Gxu-pVZX3=s360-rw'
     },
 ];
