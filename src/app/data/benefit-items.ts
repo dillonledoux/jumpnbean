de8f802341377c626fb282a229fe52a120b09a79
Dillon LeDoux <dillon.ledoux@me.com>
2021-07-19 22:26:16 -0500

    add vpd

--- src/app/data/benefit-items.ts
+++ src/app/data/benefit-items.ts
@@ -2,15 +2,9 @@ import { Benefit_Attr } from '../models/benefit-attr';
 
 export const BENEFIT_ITEMS: Benefit_Attr[] = [
     {
-        name: 'Kiwi',
-        photo: 'https://s3.amazonaws.com/www.jumpnbeanpharms.net/images/white-daisy.jpg',
-        link: '/test',
-        desc: 'Aenean aliquam dolor ex, non tempor enim accumsan eget. Cras sagittis, urna et lobortis lacinia, ante magna venenatis sapien, ac semper nunc nisl at ex. In id diam quis leo ultrices venenatis a sit amet nulla. Nunc quis est sit amet metus convallis faucibus molestie nec metus. Morbi porttitor nunc ex, malesuada tempus orci dapibus vel. Donec in fermentum risus. Fusce nec nisi quis felis faucibus ullamcorper id eu nibh. Quisque luctus volutpat suscipit. Aenean nec orci id erat maximus vestibulum. Nam eget tortor non orci fringilla consequat in vitae ante.'
+        name: 'Vapor Pressure Deficit (VPD)',
+        photo: 'assets/images/vpd-chart.jpg',
+        link: 'https://www.alchimiaweb.com/blogen/vapor-pressure-deficit-cannabis-cultivation/',
+        desc: '',
     },
-    {
-        name: 'Blueberry',
-        photo: 'https://s3.amazonaws.com/www.jumpnbeanpharms.net/images/white-daisy.jpg',
-        link: '/test',
-        desc: 'Aenean aliquam dolor ex, non tempor enim accumsan eget. Cras sagittis, urna et lobortis lacinia, ante magna venenatis sapien, ac semper nunc nisl at ex. In id diam quis leo ultrices venenatis a sit amet nulla. Nunc quis est sit amet metus convallis faucibus molestie nec metus. Morbi porttitor nunc ex, malesuada tempus orci dapibus vel. Donec in fermentum risus. Fusce nec nisi quis felis faucibus ullamcorper id eu nibh. Quisque luctus volutpat suscipit. Aenean nec orci id erat maximus vestibulum. Nam eget tortor non orci fringilla consequat in vitae ante.'
-    }
 ];
