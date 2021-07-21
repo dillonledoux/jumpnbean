2b55b28f65c6012a8b5e7fe00bdb8858dc4e4924
Dillon LeDoux <dillon.ledoux@me.com>
2021-07-20 23:19:50 -0500

    syntax errors fixed

--- src/app/data/benefit-items.ts
+++ src/app/data/benefit-items.ts
@@ -2,7 +2,7 @@ import { Benefit_Attr } from '../models/benefit-attr';
 
 export const BENEFIT_ITEMS: Benefit_Attr[] = [
     {
-        name: 'Vapor Pressure Deficit (VPD)',
+        name: 'Vapor Pressure Density (VPD)',
         photo: 'assets/images/vpd-chart.jpg',
         link: 'https://www.alchimiaweb.com/blogen/vapor-pressure-deficit-cannabis-cultivation/',
         desc: '',
