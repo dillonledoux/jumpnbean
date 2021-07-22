ccd7817be8007b6836f47258300def1ad8e42acc
Dillon LeDoux <dillon.ledoux@me.com>
2021-07-21 22:41:33 -0500

    change back to deficit

--- src/app/data/benefit-items.ts
+++ src/app/data/benefit-items.ts
@@ -2,7 +2,7 @@ import { Benefit_Attr } from '../models/benefit-attr';
 
 export const BENEFIT_ITEMS: Benefit_Attr[] = [
     {
-        name: 'Vapor Pressure Density (VPD)',
+        name: 'Vapor Pressure Deficit (VPD)',
         photo: 'assets/images/vpd-chart.jpg',
         link: 'https://www.alchimiaweb.com/blogen/vapor-pressure-deficit-cannabis-cultivation/',
         desc: '',
